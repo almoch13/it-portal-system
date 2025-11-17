da66f14d7f7643d554990f7521a0d11ff0812088
Mochammad Al Faruqi <almoch9@gmail.com>
2025-11-17 22:40:55 +0700

    Setup Tailwind + DaisyUI

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+// @ts-nocheck
+
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+import daisyui from "daisyui";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+
+    plugins: [
+        forms,
+        daisyui
+    ],
+
+    daisyui: {
+        themes: ["light", "dark", "cupcake"], // boleh pilih
+    },
+};
